3c066dbb76397150cf138f547069fa931fc47412
Nastia <bezvozmezdno@inbox.ru>
2026-01-12 15:23:58 +0300

    Add remote driver

--- src/test/java/base/BaseUITest.java
+++ src/test/java/base/BaseUITest.java
@@ -1,14 +1,20 @@
 package base;
 
 import configs.TestPropertiesConfig;
+import io.qameta.allure.Allure;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.Map;
 
 public abstract class BaseUITest {
     protected WebDriver driver;
@@ -17,7 +23,7 @@ public abstract class BaseUITest {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        initDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(2));
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
@@ -26,5 +32,25 @@ public abstract class BaseUITest {
     @AfterEach
     void tearDown() {
         driver.quit();
+    }
+
+    private void initDriver() {
+        String remoteUrl = System.getenv("SELENIUM_REMOTE_URL");
+        if (remoteUrl != null && !remoteUrl.isEmpty()) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");  // Add headless mode
+            options.addArguments("--disable-gpu"); // Switch off GPU, because we don't need it in headless mode
+            options.addArguments("--no-sandbox"); // Switch off sandbox to prevent access rights issues
+            options.addArguments("--disable-dev-shm-usage"); // Use /tmp instead of /dev/shm
+            options.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
+            try {
+                driver = new RemoteWebDriver(new URL(remoteUrl), options);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException("Malformed URL for Selenium Remote WebDriver", e);
+            }
+        } else {
+            Allure.addAttachment("Local run", "No remote driver");
+            driver = new ChromeDriver();
+        }
     }
 }
