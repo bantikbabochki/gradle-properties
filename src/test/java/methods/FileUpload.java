e9dae948e13ec14747efcfbdb2458e4d5c52df18
Nastia <bezvozmezdno@inbox.ru>
2026-01-15 16:55:35 +0300

    Добавлены методы на загрузку файла с учетом типа запуска

--- src/test/java/methods/FileUpload.java
+++ src/test/java/methods/FileUpload.java
@@ -1,5 +1,12 @@
 package methods;
 
+import base.BaseUITest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.LocalFileDetector;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -7,8 +14,16 @@ import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class FileUpload  {
+public class FileUpload {
+    private final WebDriver driver;
 
+    public FileUpload(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    /**
+     * Получить абсолютный путь к файлу из resources
+     */
     public static String getPath(String fileName) throws URISyntaxException, IOException {
         //Получаем ресурс по имени файла
         URL resource = FileUpload.class.getClassLoader().getResource(fileName);
@@ -20,6 +35,34 @@ public class FileUpload  {
         // Читаем содержимое
         return path.toAbsolutePath().toString();
     }
+
+    /**
+     * Загрузить файл в input - работает локально и удаленно
+     */
+    public void uploadFile(WebElement fileInput, String absolutePath) {
+        File file = new File(absolutePath);
+        if (!file.exists()) {
+            throw new IllegalArgumentException("Файл не найден: " + absolutePath);
+        }
+        if (BaseUITest.IS_REMOTE) {
+            ((RemoteWebDriver) driver).setFileDetector(new LocalFileDetector());
+            System.out.println("RemoteWebDriver detected, using LocalFileDetector");
+        } else {
+            System.out.println("Local WebDriver detected, uploading directly");
+        }
+        fileInput.sendKeys(absolutePath);
+        System.out.println("Файл загружен: " + file.getName());
+    }
+
+    /**
+     * Удобный метод: получить путь и сразу выгрузить файл из resources
+     */
+    public void uploadFileFromResources(WebElement fileInput, String fileName)
+            throws URISyntaxException, IOException {
+        String absolutePath = getPath(fileName);
+        uploadFile(fileInput, absolutePath);
+    }
+}
     /*
      public static String readFileAsString(String fileName) throws IOException, URISyntaxException {
         Path path = Paths.get(FileUpload.class.getClassLoader().getResource(fileName).toURI());
@@ -41,4 +84,4 @@ public class FileUpload  {
         return FileUpload.class.getClassLoader().getResource(fileName) != null;
     }
      */
-}
+
