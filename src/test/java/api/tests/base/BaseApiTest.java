daf77ff315fdf82a4b41ee62bacf71af5430ceb1
Nastia <bezvozmezdno@inbox.ru>
2026-01-24 19:39:51 +0300

    Добавлен базовый класс, для отслеживания логов

--- src/test/java/api/tests/base/BaseApiTest.java
+++ src/test/java/api/tests/base/BaseApiTest.java
@@ -1,4 +1,16 @@
 package api.tests.base;
 
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import org.junit.jupiter.api.BeforeAll;
+
 public class BaseApiTest {
+    @BeforeAll
+    static void setup() {
+        RestAssured.filters(
+                new RequestLoggingFilter(),
+                new ResponseLoggingFilter()
+        );
+    }
 }
