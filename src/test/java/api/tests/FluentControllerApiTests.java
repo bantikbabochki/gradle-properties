e114036caab8c97e45658b305a117febad424916
Nastia <bezvozmezdno@inbox.ru>
2026-01-24 21:38:21 +0300

    Добавлен коммент, как очищать id

--- src/test/java/api/tests/FluentControllerApiTests.java
+++ src/test/java/api/tests/FluentControllerApiTests.java
@@ -12,10 +12,19 @@ import static api.testdata.TestData.DEFAULT_USER;
 @Tag("API")
 public class FluentControllerApiTests extends BaseApiTest {
     FluentUserController fluentUserController = new FluentUserController();
+    //Чтобы очищались id-шники после создания
+    //List<Integer> ids = new ArrayList<>();
+
     @BeforeEach
     @AfterEach
     void clear() {
         fluentUserController.deleteUser(DEFAULT_USER.getUsername());
+        //Если бы было удаление юзера по id, то
+        /*
+        fluentUserController.deleteUser(DEFAULT_USER.getId());
+        for(int id ^ ids) {
+        fluentUserController.deleteUser(id);
+         */
     }
 
     @Test
@@ -52,4 +61,26 @@ public class FluentControllerApiTests extends BaseApiTest {
                 .jsonValueIs("username", DEFAULT_USER.getUsername())
                 .jsonValueIs("email", DEFAULT_USER.getEmail());
     }
+
+    /*
+        @Test
+    @DisplayName("Create superhero without phone, check status code and response body")
+    void checkSuperheroWithoutPhoneTest() throws InterruptedException {
+        int id = Integer.parseInt(fluentSuperheroController.createSuperhero(SUPERHERO_WITHOUT_PHONE)
+                .statusCodeIs(200)
+                .getJsonValue("id"));
+
+        Thread.sleep(3000);
+        ids.add(id); !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        fluentSuperheroController.getSuperheroesById(id)
+                .statusCodeIs(200)
+                .jsonValueIs("birthDate", SUPERHERO_WITHOUT_PHONE.getBirthDate())
+                .jsonValueIs("city", SUPERHERO_WITHOUT_PHONE.getCity())
+                .jsonValueIs("fullName", SUPERHERO_WITHOUT_PHONE.getFullName())
+                .jsonValueIs("gender", String.valueOf(SUPERHERO_WITHOUT_PHONE.getGender()))
+                .jsonValueIs("id", String.valueOf(id))
+                .jsonValueIs("mainSkill", SUPERHERO_WITHOUT_PHONE.getMainSkill())
+                .jsonValueIs("phone", null);
+    }
+     */
 }
