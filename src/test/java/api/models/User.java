19c50887dd4a8e0ed1c5424d1600adad9e98db15
Nastia <bezvozmezdno@inbox.ru>
2026-01-24 19:40:23 +0300

    Добавлена модель User

---
+++ src/test/java/api/models/User.java
@@ -0,0 +1,21 @@
+package api.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    private long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private Integer userStatus;
+}
