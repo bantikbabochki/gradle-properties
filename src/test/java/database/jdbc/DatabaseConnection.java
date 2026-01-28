95fcb21a99d9912f306146d85488dedcaedd6e6d
Nastia <bezvozmezdno@inbox.ru>
2026-01-28 20:14:48 +0300

    Добавлен класс установки соединения с бд

---
+++ src/test/java/database/jdbc/DatabaseConnection.java
@@ -0,0 +1,31 @@
+package database.jdbc;
+
+import lombok.SneakyThrows;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    private static Connection connection = null;
+
+    private static final String DB_HOST = "jdbc:h2:mem:myDb";
+    private static final String DB_USER = "sa";
+    private static final String DB_PASSWORD = "sa";
+
+    @SneakyThrows
+    public static synchronized Connection createConnection() {
+        // synchronized гарантирует, что только один поток выполняет метод
+        if (connection == null) {
+            Class.forName("org.h2.Driver");
+            connection = DriverManager.getConnection(DB_HOST, DB_USER, DB_PASSWORD);
+        }
+        return connection;
+    }
+
+    @SneakyThrows
+    public static void closeConnection() {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+}
